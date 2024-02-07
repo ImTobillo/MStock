8e0d244dcda9cc1d31afb601e8664902d88de521
ImTobillo <87785733+ImTobillo@users.noreply.github.com>
2024-02-06 23:43:30 -0300

    Agregar archivos de proyecto.

---
+++ MStock/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MStock.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
